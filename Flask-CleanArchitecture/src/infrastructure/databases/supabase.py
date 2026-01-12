50bb86ae179a473d43d793bad5c6692a77c08e8c
KhanhHung <vukhanhhung2709@gmail.com>
2026-01-12 20:43:32 +0700

    xoa cac data khong can thiet, chi su dung supabase

--- Flask-CleanArchitecture/src/infrastructure/databases/supabase.py
+++ Flask-CleanArchitecture/src/infrastructure/databases/supabase.py
@@ -1,13 +1,12 @@
-from sqlalchemy.orm import declarative_base
-
-Base = declarative_base()
-
 import os
 from supabase import create_client, Client
 
 url: str = os.environ.get("https://jednbjdohmtdxegcgunq.supabase.co")
 key: str = os.environ.get("sb_publishable_3vVjdXA9o9FSj03zZHI60A_ySK6lSnh")
 supabase: Client = create_client(url, key)
+
+def get_supabase_client() -> Client:
+    return supabase #xuat ra mot client de su dung
 
 # ORM: object relational mapping base class
 # OOP : object oriented programming
