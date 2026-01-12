601a3baee76631a6078f4228417cb8815edab8b2
KhanhHung <vukhanhhung2709@gmail.com>
2026-01-12 14:17:56 +0700

    ket noi du an voi supabase

--- Flask-CleanArchitecture/src/infrastructure/databases/base.py
+++ Flask-CleanArchitecture/src/infrastructure/databases/base.py
@@ -2,6 +2,12 @@ from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
+import os
+from supabase import create_client, Client
+
+url: str = os.environ.get("https://jednbjdohmtdxegcgunq.supabase.co")
+key: str = os.environ.get("sb_publishable_3vVjdXA9o9FSj03zZHI60A_ySK6lSnh")
+supabase: Client = create_client(url, key)
 
 # ORM: object relational mapping base class
 # OOP : object oriented programming
